4fb4c4b0ee112c880d0e8e455a9c270699733674
ispan <ispan@DESKTOP-PAKSETB>
2022-10-24 13:10:40 +0800

    加入專案檔案。

---
+++ Exec_DeclareClass/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_DeclareClass
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+	class Address
+	{
+		public string City(string city) { return city; }
+		public string ZipCode(string zipcode) { return zipcode; }
+		public string Street(string street) { return street; }
+
+	}
+
+	class Telephone
+	{
+		public string CountryCode(string country) { return country; }
+		public string AreaCode(string areacode) { return areacode; }
+		public string Number(string number) { return number; }
+		public string Ext(string ext) { return ext; }
+	}
+	class Member
+	{
+		public string Name(string name) { return name; }
+		public string Account(string account) { return account; }
+		public string Password(string password) { return password; }
+		public string Email(string email) { return email; }
+		public Address Address(Address address) { return address; }
+		public string CeilPhone(string cellphone) { return cellphone; }
+		public Telephone telephone(Telephone telephone) { return telephone; }
+
+
+		public void Register(string Name, string Account, string Password, string ConfirmPassword, string Email)
+		{
+
+		}
+		public void ForgetPassword(string name, string Email)
+		{
+
+		}
+		public void Authenticate(string Account, string Password)
+		{
+
+		}
+
+
+
+
+	}
+}
